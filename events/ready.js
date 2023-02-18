85d1fecedb9bced767a676a920487d9806ef4221
Luqman Patel <luqman.patel@gmail.com>
2023-02-17 19:29:26 -0500

    set presence when starting

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: "ready",
     run: async (bot) => {
+        const {ActivityType} = require('discord.js')
         console.log(`logged in as ${bot.client.user.tag}`)
+        bot.client.user.setPresence({ status: 'dnd', activities: [{ name: 'for messages that start with c', type: ActivityType.Watching }]});
     }
 }
