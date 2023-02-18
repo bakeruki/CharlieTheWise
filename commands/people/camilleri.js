b1742e05dc0784a129f55ad5a9e2162b670c05c6
Luqman Patel <luqman.patel@gmail.com>
2023-02-17 19:51:57 -0500

    camilleri command

---
+++ commands/people/camilleri.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "camilleri",
+    category: "people",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        message.reply('No... I cannot... He who shall not be named must never be spoken of here again!')
+    }
+}
