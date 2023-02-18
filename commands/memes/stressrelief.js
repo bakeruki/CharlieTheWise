e434f30b4f21e02bae9ade898d7fe44e242fe3cc
Luqman Patel <luqman.patel@gmail.com>
2023-02-17 19:29:19 -0500

    adding gifs to sress relief

--- commands/memes/stressrelief.js
+++ commands/memes/stressrelief.js
@@ -7,7 +7,8 @@ module.exports = {
         const gifs = ['https://tenor.com/view/samoyed-dog-profile-picture-ear-flap-cute-dog-gif-19962655', 
         'https://tenor.com/view/off-work-run-penguin-gif-22409750', 
         'https://tenor.com/view/cli%C3%A7-cli%C3%A7monkey-monkey-sleepy-sleepy-monkey-gif-24594700', 
-        'https://tenor.com/view/cat-wet-shower-bath-water-pouring-gif-17140437']
+        'https://tenor.com/view/cat-wet-shower-bath-water-pouring-gif-17140437', 
+        'https://tenor.com/view/dancing-duck-gif-24672717']
 
         const rand = Math.round(Math.random() * gifs.length)
         message.reply(`Here's a cute gif to cheer you up :) \n` + gifs[rand])
