7f2ffb95c9f39ca720a909e9e4502376c9a6f7df
Luqman Patel <luqman.patel@gmail.com>
2023-02-17 20:50:06 -0500

    update presence

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
     run: async (bot) => {
         const {ActivityType} = require('discord.js')
         console.log(`logged in as ${bot.client.user.tag}`)
-        bot.client.user.setPresence({ status: 'dnd', activities: [{ name: 'for messages that start with c', type: ActivityType.Watching }]});
+        bot.client.user.setPresence({ status: 'dnd', activities: [{ name: 'c help', type: ActivityType.Watching }]});
     }
 }
