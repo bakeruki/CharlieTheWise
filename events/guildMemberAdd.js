8d52b779ba408d4c95d08c3b4e9e4b4a09e352bb
Luqman Patel <luqman.patel@gmail.com>
2023-02-20 10:59:24 -0500

    update welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "guildMemberAdd",
     run: async (bot, member) => {
         const welcomeChannelId = "1076243806271311933"
-        member.guild.channels.cache.get(welcomeChannelId).send( `<@${member.id}> joined the server. Welcome!`)
+        member.guild.channels.cache.get(welcomeChannelId).send( `<@${member.id}> joined the server. Welcome! Please use the command 'c grade' in <#1076332274901069885> to give yourself roles!`)
     }
 }
