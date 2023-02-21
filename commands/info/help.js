f313045fbad537725a6f854a660d4d8a244582a9
Luqman Patel <luqman.patel@gmail.com>
2023-02-20 20:37:56 -0500

    update help message formatting

--- commands/info/help.js
+++ commands/info/help.js
@@ -5,19 +5,36 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
+        const { EmbedBuilder } = require('discord.js')
+
         let helpmsg = ""
         let currentcategory = ""
 
-        helpmsg += `Thank you for actually using me (seriously luqman spent way too much time programming this piece of shit). Use all of the following commands by putting a 'c' before them. \n`
+        const embed = new EmbedBuilder()
+            .setTitle("All Commands")
+            .setColor(0x0099FF)
+            .setThumbnail('https://cdn.discordapp.com/avatars/1076234325940523099/87762e183647054454931ac39f7eb5fb.webp')
+            .addFields({name: `Charlie's Note`, value: `Thank you for actually using me! (seriously an ungodly amount of time was spent programming me). Use all of the following commands by placing a 'c' before them (spaces don't matter :D).`})
+
+        // helpmsg += `Thank you for actually using me (seriously luqman spent way too much time programming this piece of shit). Use all of the following commands by putting a 'c' before them. \n`
 
         for(const command of client.commands){
             const currentcmd = command[1]
+
             if(currentcmd.category == 'dev') continue
-            if(currentcategory != currentcmd.category){
+
+            if(currentcategory == ``){
                 currentcategory = currentcmd.category
-                helpmsg += `***__${currentcmd.category} commands__***\n`
             }
+
+            if(currentcategory != currentcmd.category){
+                embed.addFields({name: `${currentcategory} commands`, value: helpmsg})
+                currentcategory = currentcmd.category
+                helpmsg = ``
+            }
+
             helpmsg += `**${currentcmd.name}** - `
+            
             if(!currentcmd.description){
                 helpmsg+=`No description available for this command (tell luqman he sucks ass at programming)\n`
             }else{
@@ -25,6 +42,25 @@ module.exports = {
             }
         }
 
-        message.reply(helpmsg)
+        // for(const command of client.commands){
+        //     const currentcmd = command[1]
+
+        //     if(currentcmd.category == 'dev') continue
+
+        //     if(currentcategory != currentcmd.category){
+        //         currentcategory = currentcmd.category
+        //         helpmsg += `***__${currentcmd.category} commands__***\n`
+        //     }
+
+        //     helpmsg += `**${currentcmd.name}** - `
+            
+        //     if(!currentcmd.description){
+        //         helpmsg+=`No description available for this command (tell luqman he sucks ass at programming)\n`
+        //     }else{
+        //         helpmsg += `${currentcmd.description}\n`
+        //     }
+        // }
+
+        message.reply({embeds: [embed]})
     }
 }
